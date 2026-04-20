02238f28c9daf5708b2ea8371d1b4f3f8f9a0122
GoldenSpringFox <46161299+GoldenSpringFox@users.noreply.github.com>
2026-04-20 16:41:03 +0300

    added ThreadIdManager back

--- Ex1/uthreads.cpp
+++ Ex1/uthreads.cpp
@@ -143,3 +143,55 @@ int uthread_get_quantums(int tid) {
     std::cerr << "thread library error: " << "did not implement" << std::endl;
     return -1;
 }
+
+
+/**************************************************
+*                                                 *
+*                   Classes                       *
+*                                                 *
+***************************************************/
+
+class ThreadIdManager {
+    public:
+         
+            int getNewThreadId(){
+                if (!terminatedThreads.empty()) {
+                    auto smallestIdPointer = terminatedThreads.begin();
+                    int smallestId = *(smallestIdPointer);
+                    terminatedThreads.erase(smallestId);
+                    return smallestId;
+                }
+
+                if (currentMaxId < MAX_THREAD_NUM) {
+                    currentMaxId++;
+                    return currentMaxId;
+                }
+                
+                std::cerr << "ERROR: passed max threads number\n";
+                return -1;
+        
+            }
+
+
+        int removeThread(int id){
+            if (id == currentMaxId) {
+                currentMaxId--;
+                while (terminatedThreads.count(currentMaxId)>0) {
+                    terminatedThreads.erase(currentMaxId);
+                    currentMaxId--;
+                }
+                return 0;
+            }
+
+            if ((terminatedThreads.count(id) > 0) || (id < 0) || (id > currentMaxId)) {
+                return -1;
+            }
+
+            terminatedThreads.insert(id);
+            return 0;
+        }
+
+    private: 
+        std::set <int> terminatedThreads;
+        int currentMaxId; 
+};
